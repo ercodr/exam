d59d783a59b53de2aaf2547a69dd60c6348859ef
ercodr <82173838+Dahcodingkid@users.noreply.github.com>
2021-07-03 12:37:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -91,19 +91,15 @@ var month = shortMonthName(date); // "Jul"
 
 // -------------To CHANGE Button Color --------- //
 if(day == 20 && month === 'Jul' && year == 2021){
-    document.getElementById('d1').style.backgroundColor = '#ffbb00';
-    document.getElementById('d1').style.color = '#000000';
-    document.getElementById('d1').style.fontWeight = 'bold';
+    document.getElementById('d1').classList.add('active');
     detail[0].innerText = 'Project Management';
     detail[1].innerText = 'Tuesday 20th July, 2021';
     detail[2].innerText = '8:00am - 10:00am';
     detail[3].innerText = 'Basement';
 }
 
-if(day == 20 && month === 'Jul' && year == 2021){
-    document.getElementById('d1').style.backgroundColor = '#ffbb00';
-    document.getElementById('d1').style.color = '#000000';
-    document.getElementById('d1').style.fontWeight = 'bold';
+if(day == 22 && month === 'Jul' && year == 2021){
+    document.getElementById('d2').classList.add('active');
     detail[0].innerText = 'Software Engineering';
     detail[1].innerText = 'Thursday 22nd July, 2021';
     detail[2].innerText = '8:00am - 10:00am';
@@ -111,9 +107,7 @@ if(day == 20 && month === 'Jul' && year == 2021){
 }
 
 if(day == 24 && month === 'Jul' && year == 2021){
-    document.getElementById('d1').style.backgroundColor = '#ffbb00';
-    document.getElementById('d1').style.color = '#000000';
-    document.getElementById('d1').style.fontWeight = 'bold';
+    document.getElementById('d3').classList.add('active');
     detail[0].innerText = 'Database (Oracle)';
     detail[1].innerText = 'Saturday 24th July, 2021';
     detail[2].innerText = '8:00am - 10:00am';
@@ -121,9 +115,7 @@ if(day == 24 && month === 'Jul' && year == 2021){
 }
 
 if(day == 27 && month === 'Jul' && year == 2021){
-    document.getElementById('d1').style.backgroundColor = '#ffbb00';
-    document.getElementById('d1').style.color = '#000000';
-    document.getElementById('d1').style.fontWeight = 'bold';
+    document.getElementById('d4').classList.add('active');
     detail[0].innerText = 'Research Method';
     detail[1].innerText = 'Tuesday 27th July, 2021';
     detail[2].innerText = '8:00am - 10:00am';
@@ -131,9 +123,7 @@ if(day == 27 && month === 'Jul' && year == 2021){
 }
 
 if(day == 29 && month === 'Jul' && year == 2021){
-    document.getElementById('d1').style.backgroundColor = '#ffbb00';
-    document.getElementById('d1').style.color = '#000000';
-    document.getElementById('d1').style.fontWeight = 'bold';
+    document.getElementById('d5').classList.add('active');
     detail[0].innerText = 'Data Comm II';
     detail[1].innerText = 'Thursday 29th July, 2021';
     detail[2].innerText = '8:00am - 10:00am';
@@ -141,59 +131,58 @@ if(day == 29 && month === 'Jul' && year == 2021){
 }
 
 if(day == 31 && month === 'Jul' && year == 2021){
-    document.getElementById('d1').style.backgroundColor = '#ffbb00';
-    document.getElementById('d1').style.color = '#000000';
-    document.getElementById('d1').style.fontWeight = 'bold';
+    document.getElementById('d6').classList.add('active');
     detail[0].innerText = 'Operating System';
     detail[1].innerText = 'Saturday 31st July, 2021';
     detail[2].innerText = '2:00pm - 4:00pm';
     detail[3].innerText = 'Basement';
 }
 
+// ----------------------------------------------------------------- //
+btn.forEach(button => {
+    if(button.textContent === 'Completed!'){
+        button.classList.add('alert');
+    }
+});
 
 
 
 // ---------------------To REMOVE Button---------------------------- //
-if(day == 20 + nextDay && month === 'Jul' && year == 2021){
+if(day > 20 && month === 'Jul' && year == 2021){
     btn[0].classList.add('completed');
     btn[0].innerText = 'Completed!'
-    btn[0].style.color = 'red';
-    btn[0].style.backgroundColor = '#000';
 }
 
-if(day == 22 + nextDay && month === 'Jul' && year == 2021){
+if(day > 22 && month === 'Jul' && year == 2021){
     btn[1].classList.add('completed');
     btn[1].innerText = 'Completed!'
-    btn[1].style.color = 'red';
-    btn[1].style.backgroundColor = '#000';
 }
 
-if(day == 24 + nextDay && month === 'Jul' && year == 2021){
+if(day > 24 && month === 'Jul' && year == 2021){
     btn[2].classList.add('completed');
     btn[2].innerText = 'Completed!'
-    btn[2].style.color = 'red';
-    btn[2].style.backgroundColor = '#000';
 }
 
-if(day == 27 + nextDay && month === 'Jul' && year == 2021){
+if(day > 27 && month === 'Jul' && year == 2021){
     btn[3].classList.add('completed');
     btn[3].innerText = 'Completed!'
-    btn[3].style.color = 'red';
-    btn[3].style.backgroundColor = '#000';
 }
 
-if(day == 29 + nextDay && month === 'Jul' && year == 2021){
+if(day > 29 && month === 'Jul' && year == 2021){
     btn[4].classList.add('completed');
     btn[4].innerText = 'Completed!'
-    btn[4].style.color = 'red';
-    btn[4].style.backgroundColor = '#000';
 }
 
-if(day == 31 + nextDay && month === 'Jul' && year == 2021){
+if(day > 31 && month === 'Jul' && year == 2021){
     btn[5].classList.add('completed');
     btn[5].innerText = 'Completed!'
-    btn[5].style.color = 'red';
-    btn[5].style.backgroundColor = '#000';
+    
 }
 
+let span = document.querySelectorAll('.alert');
 
+if(day < 20){
+    span.forEach(button => {
+        button.textContent = 'Upcoming!';
+    });
+}
